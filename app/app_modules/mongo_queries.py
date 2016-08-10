df9721a3ba60a5c3ebbdc6f4bcff78e16ee0c5f6
vujke <stefanvujovic93@gmail.com>
2016-08-10 21:50:37 +0200

    rendering data in template

--- app/app_modules/mongo_queries.py
+++ app/app_modules/mongo_queries.py
@@ -1,5 +1,6 @@
 import mongo_connection
 import pprint
+from bson.son import SON
 
 db, collection = mongo_connection.connect_client('jobs_db','job_posts')
 
@@ -14,42 +15,43 @@ one = collection.find_one()
 pprint.pprint(one)
 
 # printing the number of documents with city: Beograd and Belgrade
-
+def count_by_key(key, value):
+    return collection.find({key:value}).count()
 beograd = collection.find({"city":"Beograd"}).count()
-print "Oglasi iz Beograda"
-pprint.pprint(beograd)
+# print "Oglasi iz Beograda"
+# pprint.pprint(beograd)
 
 belgrade = collection.find({"city":"Belgrade"}).count()
-print "Oglasi iz Belgrade"
-pprint.pprint(belgrade)
+# print "Oglasi iz Belgrade"
+# pprint.pprint(belgrade)
 
 belgrade = collection.find({"city":{ '$in': ["Belgrade", "Beograd"]}}).count()
-print "Oglasi iz Belgrade i Beograd"
-pprint.pprint(belgrade)
+# print "Oglasi iz Belgrade i Beograd"
+# pprint.pprint(belgrade)
 
 # printing number of jobs with java tag query for list
 java = collection.find({"tags":"java"}).count()
-print "Oglasi sa tagom Java:"
-pprint.pprint(java)
+# print "Oglasi sa tagom Java:"
+# pprint.pprint(java)
 
 # unique values for companies
 unique = collection.distinct("firm")
 #for firm in unique:
 #    pprint.pprint(firm)
-print "No. of companies that advertise positions"
-print len(unique)
-print unique
+# print "No. of companies that advertise positions"
+# print len(unique)
+# print unique
 
 unique = collection.distinct("tags")
 #for firm in unique:
 #    pprint.pprint(firm)
-print "No. of unique tags"
-print len(unique)
-print unique
+# print "No. of unique tags"
+# print len(unique)
+# print unique
 
 #https://api.mongodb.com/python/current/examples/aggregation.html
 
-from bson.son import SON
+
 pipeline = [
     {"$unwind": "$tags"},
     {"$group": {"_id": "$tags", "count": {"$sum": 1}}},
@@ -60,13 +62,24 @@ pipeline = [
 print "Frekvencijski recnik"
 print list(collection.aggregate(pipeline))
 
-from bson.son import SON
+
 pipeline = [
     {"$unwind": "$city"},
     {"$group": {"_id": "$city", "count": {"$sum": 1}}},
      {"$sort": SON([("count", -1), ("_id", -1)])}
  ]
-#list(db.things.aggregate(pipeline))
 
-print "Frekvencijski recnik"
-print list(collection.aggregate(pipeline))
+def top_x(x,key1,value,dkey):
+
+    pipeline = [
+    {"$match": {key1: value}},
+    {"$unwind": dkey},
+    {"$group": {"_id": dkey, "count": {"$sum": 1}}},
+    {"$sort": SON([("count", -1), ("_id", -1)])}
+    ]
+    return list(collection.aggregate(pipeline))[:x]
+
+# print "Frekvencijski recnik"
+# print list(collection.aggregate
+
+count_by_key("city","Belgrade")
--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from app_modules import mongo_queries
 
+
+def key_modificator(x_list):
+    for i in x_list:
+        i["id"] = i["_id"]
 # Create your views here.
 def home(request):
 
+    add_num = mongo_queries.count_by_key("date","29/7/16")
+    top_tags = mongo_queries.top_x(10,"date","29/07/16","$tags")
+    top_cities = mongo_queries.top_x(5,"date","29/07/16","$city")
+    top_firms = mongo_queries.top_x(10,"date","29/07/16","$firm")
+    top_positions = mongo_queries.top_x(10,"date","29/07/16","$positions")
+
+    key_modificator(top_tags)
+    key_modificator(top_cities)
+    key_modificator(top_positions)
+    key_modificator(top_firms)
+
     context = {
-        "test": "Test"
+        "test": "Test",
+        "add_num": add_num,
+        "top_tags": top_tags,
+        "top_cities": top_cities,
+        "top_firms": top_firms,
+        "top_positions": top_positions,
     }
 
     return render(request, 'home.html', context)
